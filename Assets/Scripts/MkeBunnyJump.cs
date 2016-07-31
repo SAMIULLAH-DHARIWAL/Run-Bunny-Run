fa04001e4fadf21a02a35316315112e0cc81e002
SAMIULLAH-DHARIWAL <samiullah.dhariwal@outlook.com>
2016-07-31 08:37:43 +0500

    Initial Scene

---
+++ Assets/Scripts/MkeBunnyJump.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MkeBunnyJump : MonoBehaviour {
+	public Rigidbody2D rb;
+	public float jumpForce;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp("space")){
+			rb.AddForce(transform.up * jumpForce);
+		}
+	}
+}
